ec9a3d8fa41ce14f38970d492f01ced94827d178
srivastavaayush084 <srivastavaayush084@gmail.com>
2026-08-07 09:05:22 +0530

    chore: update creator sign-up Google Form link

--- src/routes/join-creator.tsx
+++ src/routes/join-creator.tsx
@@ -2,7 +2,7 @@ import { createFileRoute } from "@tanstack/react-router";
 import { SiteLayout } from "@/components/site/SiteLayout";
 import { useEffect } from "react";
 
-const GOOGLE_FORM_URL = "https://forms.gle/apmG7JsqUbiKknNHA";
+const GOOGLE_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSeW3h5M-v6ftUB9lVezqAv_o9RIWOimxyVT_-SMlsYAIj-uCg/viewform";
 
 export const Route = createFileRoute("/join-creator")({
   head: () => ({
